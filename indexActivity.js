c694ee9811f917dd9c077795cc7ec204e36cc834
Moonbeaam <therandomanything@gmail.com>
2023-03-21 15:27:20 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let fName = "Marc";
+let lName = "Agaceta";
+let age = 19;
+let hobbies = ["Volleyball", "Basketball", "Games", "K-pop", "Anime"];
+let address = {
+    houseNumber: "Block 1 Lot 1",
+    street: "Mongolia",
+    subd: "Springville",
+    brgy: "San Jose",
+    city: "Manila",
+    province: "Metro Manila",
+    zip: 1111    
+}
+
+console.log(fName);
+console.log(lName);
+console.log(age);
+console.log(hobbies);
+console.log(address);
